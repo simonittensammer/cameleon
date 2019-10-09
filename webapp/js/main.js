1596e4d60b0908277be75ebb726f5bc3b6e9e10c
Simon <simon.ittensammer@gmx.at>
2019-10-09 22:00:49 +0200

    added feature to open the cannel selection bar

---
+++ webapp/js/main.js
@@ -0,0 +1,35 @@
+let channelSelectionIndicator = document.getElementById("channelSelectionIndicator");
+let channelSelection = document.getElementById("channelSelection");
+let browserVideo = document.getElementById("browserVideo");
+
+let channelSelectionVisible = false;
+
+// HOVER EFFECTS
+channelSelectionIndicator.addEventListener("mouseover", function () {
+    if (!channelSelectionVisible) {
+        channelSelection.style.left = "99vw";
+        channelSelectionIndicator.style.right = "-1vw";
+    }
+})
+
+channelSelectionIndicator.addEventListener("mouseout", function () {
+    if (!channelSelectionVisible) {
+        channelSelection.style.left = "100vw";
+        channelSelectionIndicator.style.right = "-2.5vw";
+    }
+})
+
+// CLICK
+channelSelectionIndicator.addEventListener("click", function () {
+    if (channelSelectionVisible) {
+        channelSelectionVisible = false;
+        channelSelection.style.left = "100vw";
+        channelSelectionIndicator.style.right = "-2.5vw";
+        browserVideo.style.width = "100vw";
+    } else {
+        channelSelectionVisible = true;
+        channelSelection.style.left = "80vw";
+        channelSelectionIndicator.style.right = "17.5vw";
+        browserVideo.style.width = "80vw";
+    }
+})
