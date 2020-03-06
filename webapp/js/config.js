df2cac48ed96e767a2332a319be6044da7f77576
lorenzgruber <regru.lg@gmail.com>
2020-03-06 20:26:23 +0100

    add socket connection to config page

--- webapp/js/config.js
+++ webapp/js/config.js
@@ -1,6 +1,13 @@
 var activePage = document.getElementById("dashboard");
 var activePageButton = document.getElementById("dashboardButton");
 
+const socket = io();
+
+socket.on('join', data => {
+    console.log(data);
+    socket.emit('joined');
+})
+
 function changeSettingPage (page) {
     activePage.style.display = "none";
     activePageButton.classList.remove("activeSetting");
