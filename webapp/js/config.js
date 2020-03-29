758b7519978cdfae87bdd73df9539d658a4da2f2
lorenzgruber <regru.lg@gmail.com>
2020-03-29 13:42:33 +0200

    dragable objects

--- webapp/js/config.js
+++ webapp/js/config.js
@@ -1,5 +1,6 @@
 let activePage = document.getElementById('dashboard');
 let activePageButton = document.getElementById('dashboard-button');
+let imagePreview = document.getElementById('place-image-preview');
 let channelSelect = document.getElementById('channel-select');
 let selectedChannelHeadline = document.getElementById('selected-channel');
 let objectSelect = document.getElementById('object-select');
@@ -32,6 +33,56 @@ channelSelect.addEventListener('change', (event) => {
     
 });
 
+objectSelect.addEventListener('change', (event) => {
+    addObject(event.target.value);
+    event.target.value = 'default';
+});
+
+
+let objects = document.querySelectorAll('.overlay-object');
+let selectedObject = null;
+let grabbedObject = null;
+
+objects.forEach(object => {
+    object.addEventListener('click', (event) => {
+        setSelected(event.target);
+    });
+});
+
+objects.forEach(object => {
+    object.addEventListener('mousedown', (event) => {
+        grabbedObject = event.target;
+        setSelected(event.target);
+    });
+});
+
+imagePreview.addEventListener('mousemove', (event) => {  
+    if(grabbedObject != null) {
+        let left;
+        let top;
+
+        if(grabbedObject.style.left != '') {
+            left = parseInt(grabbedObject.style.left.replace('px', '')) + event.movementX;
+        } else {
+            left = event.movementX;
+        }
+        if(grabbedObject.style.top != '') {
+            top = parseInt(grabbedObject.style.top.replace('px', '')) + event.movementY;
+        } else {
+            top = event.movementY;
+        }
+
+        grabbedObject.style.left = left + "px";
+        grabbedObject.style.top = top + "px";          
+    }
+});
+
+imagePreview.addEventListener('mouseup', (event) => {  
+    if(grabbedObject != null) {
+        grabbedObject = null;
+    }
+});
+
 
 function changeSettingPage(page) {
     activePage.style.display = 'none';
@@ -42,4 +93,24 @@ function changeSettingPage(page) {
 
     activePage.style.display = 'block';
     activePageButton.classList.add('active-setting');
+}
+
+function setSelected(element) {
+
+    if(element === null) {
+        if(selectedObject != null) {
+            selectedObject.classList.remove('selected');
+        }
+        selectedObject = null;
+    } else {
+        if(selectedObject != null) {
+            selectedObject.classList.remove('selected');
+        }
+        selectedObject = element;
+            selectedObject.classList.add('selected');
+    }
+}
+
+function addObject(type) {
+    
 }
