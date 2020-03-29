e1ecc9eb71c6389c64c0fcd195ef438e7f9678ec
lorenzgruber <regru.lg@gmail.com>
2020-03-29 17:04:07 +0200

    object properties panel

--- webapp/js/config.js
+++ webapp/js/config.js
@@ -4,6 +4,9 @@ let imagePreview = document.getElementById('place-image-preview');
 let channelSelect = document.getElementById('channel-select');
 let selectedChannelHeadline = document.getElementById('selected-channel');
 let objectSelect = document.getElementById('object-select');
+let xInput = document.getElementById('x-input');
+let yInput = document.getElementById('y-input');
+let scaleInput = document.getElementById('scale-input');
 
 const socket = io();
 
@@ -30,11 +33,10 @@ socket.on('join', data => {
 
 channelSelect.addEventListener('change', (event) => {
     selectedChannelHeadline.innerText = event.target.value;
-    
 });
 
 objectSelect.addEventListener('change', (event) => {
-    addObject(event.target.value);
+    addObject(event.target.value, 0, 0, 1, false);
     event.target.value = 'default';
 });
 
@@ -42,19 +44,6 @@ objectSelect.addEventListener('change', (event) => {
 let objects = document.querySelectorAll('.overlay-object');
 let selectedObject = null;
 let grabbedObject = null;
-
-objects.forEach(object => {
-    object.addEventListener('click', (event) => {
-        setSelected(event.target);
-    });
-});
-
-objects.forEach(object => {
-    object.addEventListener('mousedown', (event) => {
-        grabbedObject = event.target;
-        setSelected(event.target);
-    });
-});
 
 imagePreview.addEventListener('mousemove', (event) => {  
     if(grabbedObject != null) {
@@ -72,6 +61,8 @@ imagePreview.addEventListener('mousemove', (event) => {
             top = event.movementY;
         }
 
+        setInputValues(left, top);
+
         grabbedObject.style.left = left + "px";
         grabbedObject.style.top = top + "px";          
     }
@@ -81,6 +72,18 @@ imagePreview.addEventListener('mouseup', (event) => {
     if(grabbedObject != null) {
         grabbedObject = null;
     }
+});
+
+xInput.addEventListener('change', (event) => {
+    selectedObject.style.left = event.target.value + 'px';
+});
+
+yInput.addEventListener('change', (event) => {
+    selectedObject.style.top = event.target.value + 'px';
+});
+
+scaleInput.addEventListener('change', (event) => {
+    selectedObject.style.transform = 'scale(' + event.target.value + ')';
 });
 
 
@@ -102,15 +105,61 @@ function setSelected(element) {
             selectedObject.classList.remove('selected');
         }
         selectedObject = null;
+        setInputValues('', '');
+        scaleInput.value = '';
+        toggleValueInputs(true);
+
     } else {
         if(selectedObject != null) {
             selectedObject.classList.remove('selected');
         }
         selectedObject = element;
-            selectedObject.classList.add('selected');
+        selectedObject.classList.add('selected');
+        setInputValues(
+            selectedObject.style.left.replace('px', ''),
+            selectedObject.style.top.replace('px', ''));
+        scaleInput.value = selectedObject.style.transform.replace('scale(', '').replace(')', '');    
+        toggleValueInputs(false);    
+        
     }
 }
 
-function addObject(type) {
+function setInputValues(x, y) {
+    xInput.value = x;
+    yInput.value = y;
+}
+
+function toggleValueInputs(disabled) {
+    xInput.disabled = disabled;
+    yInput.disabled = disabled;
+    scaleInput.disabled = disabled;
+}
+
+function addObject(type, x, y, scale, persist) {
+    let object;
     
+    if(type === 'txt') {
+        object = document.createElement('div');
+        object.classList.add('text-object');
+        object.innerText = 'Text Object';
+    }
+
+    object.classList.add('overlay-object');
+    
+    object.style.left = x + 'px';
+    object.style.top = y + 'px';
+    object.style.transform = 'scale(' + scale + ')';
+
+    object.addEventListener('click', (event) => {
+        setSelected(event.target);
+    });
+
+    object.addEventListener('mousedown', (event) => {
+        grabbedObject = event.target;
+        setSelected(event.target);
+    });
+
+    imagePreview.appendChild(object);
+
+    console.log(object);
 }
