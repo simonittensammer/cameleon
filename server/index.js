acd5518339402fe1afe97764cdfec7f3a1696421
simonittensammer <simon.ittensammer@gmx.at>
2020-04-16 11:28:31 +0200

    clean up code in index.js

--- server/index.js
+++ server/index.js
@@ -43,53 +43,6 @@ setInterval(() => {
     io.emit('image', image);
 }, 1000 / FPS);
 
-
-/*
-// POST - ADDCHANNEL
-app.post('/', urlencodedParser, function(req, res) {
-
-    // req.body.name
-    // req.body.desc
-    // req.body.url
-
-    
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("cameleonDB");
-        
-        var newCam = { name: req.body.name, desc: req.body.desc ,ip: req.body.url };
-        dbo.collection("cams").insertOne(newCam, function(err, res) {
-          if (err) throw err;
-          console.log("inserted: " + newCam.name);
-          db.close();
-        });
-        
-        dbo.collection("cams").find().toArray(function(err, result) {
-          if (err) throw err;
-          console.log(result);
-          db.close();
-        });
-    });
-    
-
-    
-    fs.readFile(path.join(__dirname, '/index.html'), 'utf8', (err,html)=>{
-        if(err){
-           throw err;
-        }
-     
-        const root = parse(html);
-     
-        const box = root.querySelector('#testBox');
-        box.appendChild('<div>' + req.body.name + '</div>');
-     
-        //console.log(root.toString()); // This you can write back to file!
-        res.send(root.toString());
-    });
-    
-});
-*/
-
 io.on('connection', socket => {
 
     // sending cam-data from db to client when connecting
