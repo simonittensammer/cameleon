b2bc8c95c2909c7848eb403e8cf3be01021598af
simonittensammer <simon.ittensammer@gmx.at>
2020-10-06 10:32:25 +0200

    add new docu structure

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -3,11 +3,21 @@
 # OR RATHER READ IT AND THEN USE
 # ONLY WORKS FROM THE PROJECT DIR
 # WILL PUBLISH STUFF ON THE WEBSITE, DON'T SCREW UP
-export old=${PWD}
-${PWD}/build-html-docker.sh
-cd docs
+BUILD_DIR="gh-pages"
+OLD=$(pwd)
+echo **** OLD=$OLD
+ORIGIN_URL=$(git config --get remote.origin.url)
+$(pwd)/build-html-docker.sh
+cd $BUILD_DIR
+git config --global user.email "simon.ittensammer@gmx.at"
+git init
 git add .
-git commit -m "update Demo Website"
-git push origin master
-cd $old
+git commit -m "$USER - rebuilding gh-pages $(date)"
+git remote add origin $ORIGIN_URL
+git push --force origin master:gh-pages
+cd $OLD
+rm -rf $BUILD_DIR
 
+# Sources:
+# maybe better with subtrees -> https://gist.github.com/cobyism/4730490
+# https://blog.bloomca.me/2017/12/15/how-to-push-folder-to-github-pages.html
