09baa9ab6429135dc9040f1febc7f6e1efdcda40
simonittensammer <simon.ittensammer@gmx.at>
2020-11-02 16:35:28 +0100

    add live-view component

---
+++ cameleon-frontend/src/app/live-view/live-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-live-view',
+  templateUrl: './live-view.component.html',
+  styleUrls: ['./live-view.component.scss']
+})
+export class LiveViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
