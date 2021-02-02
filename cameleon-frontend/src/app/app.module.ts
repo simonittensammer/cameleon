d6c26c1a8a46512ae8e6cfd8dfa1e175a62530cd
simonittensammer <simon.ittensammer@gmx.at>
2021-02-02 11:58:19 +0100

    change image to mat-icon

--- cameleon-frontend/src/app/app.module.ts
+++ cameleon-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSelectModule} from '@angular/material/select';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
     declarations: [
@@ -33,6 +34,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatSelectModule,
     MatCardModule,
     MatButtonModule,
+    MatIconModule,
     MatSelectModule
   ],
   providers: [],
